0c66643683f20d7493139c00348232396783c4a2
Dustin Lucien <dustin.lucien@gmail.com>
2013-12-01 16:07:10 -0500

    changes to support blog posts on the homepage

--- shopify/eleanors-5545737/assets/jquery.wordpress.js
+++ shopify/eleanors-5545737/assets/jquery.wordpress.js
@@ -1,7 +1,7 @@
 (function($){
   $.fn.wordpress = function(options) {
     var that = this,
-        apiEndpoint = "https://public-api.wordpress.com/rest/v1/",
+        apiEndpoint = "https://public-api.wordpress.com/rest/v1",
         settings = {
           count: null,
           url: null
@@ -10,35 +10,51 @@
     options && $.extend(settings, options);
 
     function createArticleElement(article) {
-      if((photo.caption) !== null){
-        var photo_content = photo.caption.text + "  -  ";
-      } else {
-        var photo_content = " "
-      }
-      return $('<div>')
-        .addClass('instagram-placeholder desktop-2 mobile-half')
-        .attr('id', photo.id)
+      return $('<li>')
+        .addClass('desktop-4 mobile-3')
+        .attr('id', article.ID)
         .append(
           $('<a>')
             .attr('target', '_blank')
-            .attr('href', photo.link)
-            .attr('title', photo.caption.text)
+            .attr('href', article.URL)
+            .attr('title', article.title)
             .append(
-              $('<img>')
-                .addClass('instagram-image')
-                .attr('src', photo.images.low_resolution.url)
+              $('<h3>')
+                .addClass('text-center')
+                .html(article.title)
+            )
+        )
+        .append(
+          $('<div>')
+            .attr('id', 'homepage-excerpt')
+            .html(article.excerpt)
+        )
+        .append(
+          $('<div>')
+            .addClass('text-left')
+            .append(
+              $('<a>')
+                .attr('href', article.URL)
+                .html('Read More')
+            )
+        )
+        .append(
+          $('<div>')
+            .addClass('text-right')
+            .append(
+              $('<a>')
+                .attr('href', article.URL + '#comments')
+                .html(article.comment_count + ' comments')
             )
         );
     }
-
-
 
     function composeRequestURL() {
       var url = apiEndpoint,
           params = {};
 
       if (settings.url != null) {
-        url += "/sites/" + settings.url + "/posts/";
+        url += "/sites/" + settings.url + "/posts";
       }
 
       if (settings.count != null) {
@@ -54,14 +70,14 @@
 
     $.ajax({
       type: "GET",
-      dataType: "json",
+      dataType: "jsonp",
       cache: false,
       url: composeRequestURL(),
       success: function(res) {
-        var limit = settings.count == null ? res.data.length : settings.count;
+        var limit = settings.count == null ? res.posts.length : settings.count;
 
         for(var i = 0; i < limit; i++) {
-          that.append(createArticleElement(res.data[i]));
+          that.append(createArticleElement(res.posts[i]));
         }
       }
     });
